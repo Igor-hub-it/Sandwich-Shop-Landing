c94dadbcf88f44306cb71243bdd137519d26b8b1
vi2al <syxarik.33@mail.ru>
2021-08-08 04:19:35 +0700

    scrypt1(new)

---
+++ scrypt1.js
@@ -0,0 +1,24 @@
+let add = document.querlySelector("#add");
+let list = document.querlySelector("#list");
+add.onclick = (e) => {
+    e.preventDefault();
+    let listitem = document.querySelector("#listitem");
+    if (listitem.value !== ''){
+        let el = document.createElement("li");
+        el.innerHTML = listitem.value;
+        let removebtn = document.createElement("button");
+        removebtn.className = "close";
+        removebtn.innerHTML = "\u00D7";
+        el.appendChild(removebtn);
+        list.appendChild(el);
+        listitem.value = "";
+        el.addEventListener("click", (e) => {
+            e.target.style.textDecoration = "line-through";
+        })
+        removebtn.addEventListener("click", (e) => {
+            e.target.parentElement.style.display = "none";
+        });
+    } else {
+        alert("No input\nWrite something!!");
+    }
+}
