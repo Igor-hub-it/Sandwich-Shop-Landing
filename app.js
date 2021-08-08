c6d88184cec6c9cb1a1e12d6de8c4cd41db2dbfc
IgorPin <igorpigidin@gmail.com>
2021-08-09 01:09:52 +0700

    dfghj

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 // for (let i = 1; i<2; i++){
 //     alert("привет, лошок")
 // }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const slider = new ChiefSlider('.slider', {
+      loop: true,
+      autoplay: true,
+      interval: 7000,
+    });
+  });
